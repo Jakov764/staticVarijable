8e7ad543cc0e86501840fa1cd5011b3db086da19
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2020-10-06 11:24:51 +0200

    Add files via upload

---
+++ cvekŠpraljaJakovVjezba2/cvekŠpraljaJakovVjezba2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovVjezba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa("Ivan", "Ivancic");
+            Console.WriteLine(x.ToString());
+            PrvaKlasa y = new PrvaKlasa("Pero", "Peric");
+            Console.WriteLine(y.ToString());
+            PrvaKlasa z = new PrvaKlasa();
+
+
+            z.Ime = "Milorad";
+            z.Prezime = "Milić";
+
+            Console.WriteLine(z.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cvekŠpraljaJakovVjezba2/cvekŠpraljaJakovVjezba2/PrvaKlasa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cvekŠpraljaJakovVjezba2
+{
+    class PrvaKlasa
+    {
+        private static int counter;
+        string ime;
+        string prezime;
+        public PrvaKlasa()
+        {
+            counter++;
+        }
+
+        public int Counter { get => counter;}
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+
+        public override string ToString()
+        {
+            string ispis = "Redni broj: " + counter + ", " + ime + ", " + prezime;
+            return ispis;
+        }
+
+        public PrvaKlasa(string ime, string prezime)
+        {
+            counter++;
+            this.ime = ime;
+            this.prezime = prezime;
+
+        }
+    }
+}
